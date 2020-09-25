76520d2f173332bcf4de7e265ca90002395fe1a6
David <dawid.prosba@gmail.com>
2020-09-25 22:25:04 +0200

    npm

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -12,73 +12,94 @@ const App = () => {
 
 
   return (
-    <div>
+    <div style={{ outline: "100px gray solid" }}>
 
 
 
-      <div>
-        <h1>
-          Default button
-        </h1>
 
-        <div>
-          <Button text="Default button" variety="default" classNames={[]} />
-          <ButtonPrimary />
-          <ButtonSecondary />
-          <ButtonSuccess />
-          <ButtonDanger />
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+
+        <div style={{ width: '50vw', minHeight: "100vh", padding: '1rem' }}>
+
+          <div>
+            <Button text="Default button" variety="default" classNames={[]} />
+            <ButtonPrimary />
+            <ButtonSecondary />
+            <ButtonSuccess />
+            <ButtonDanger />
+          </div>
+
+          <div>
+            <ButtonDanger text='Button - lg' size='lg' />
+            <ButtonSuccess text='Button - md' size='md' />
+            <ButtonSecondary text='Button - sm' size='sm' />
+            <ButtonPrimary text='Button - xs' size='xs' />
+          </div>
+
+
+          <div>
+            <Button text="Default button" type="outlined" variety="default" classNames={[]} />
+            <ButtonPrimary type="outlined" />
+            <ButtonSecondary type="outlined" />
+            <ButtonSuccess type="outlined" />
+            <ButtonDanger type="outlined" />
+          </div>
+
+          <div>
+            <Button text="Text button" type="text" variety="default" classNames={[]} />
+          </div>
+
+
+          <div>
+            <Button icon="&#x274C;" text="Leading icon" type="text" variety="default" />
+            <Button icon="&#x274C;" text='' type="text" variety="default" />
+            <ButtonDanger icon="&#x274C;" text="" type="outlined" />
+            <ButtonDanger icon="&#x274C;" text='Button - lg' size='lg' />
+          </div>
+
         </div>
 
-        <h2>
-          Different sizes
-        </h2>
-        <div>
-          <ButtonDanger text='Button - lg' size='lg' />
-          <ButtonSuccess text='Button - md' size='md' />
-          <ButtonSecondary text='Button - sm' size='sm' />
-          <ButtonPrimary text='Button - xs' size='xs' />
-        </div>
-        <h2>
-          Outlined buttons
-        </h2>
+        <div style={{ width: '50vw', minHeight: "100vh", backgroundColor: "#0e0e10", padding: '1rem' }}>
 
         <div>
-          <Button text="Default button" type="outlined" variety="default" classNames={[]} />
-          <ButtonPrimary type="outlined"/>
-          <ButtonSecondary type="outlined"/>
-          <ButtonSuccess type="outlined"/>
-          <ButtonDanger type="outlined"/>
+            <Button text="Default button" variety="default" classNames={[]} />
+            <ButtonPrimary />
+            <ButtonSecondary />
+            <ButtonSuccess />
+            <ButtonDanger />
+          </div>
+
+          <div>
+            <ButtonDanger text='Button - lg' size='lg' />
+            <ButtonSuccess text='Button - md' size='md' />
+            <ButtonSecondary text='Button - sm' size='sm' />
+            <ButtonPrimary text='Button - xs' size='xs' />
+          </div>
+
+
+          <div>
+            <Button text="Default button" type="outlined" variety="default" classNames={[]} />
+            <ButtonPrimary type="outlined" />
+            <ButtonSecondary type="outlined" />
+            <ButtonSuccess type="outlined" />
+            <ButtonDanger type="outlined" />
+          </div>
+
+          <div>
+            <Button text="Text button" type="text" variety="default" classNames={[]} />
+          </div>
+
+
+          <div>
+            <Button icon="&#x274C;" text="Leading icon" type="text" variety="default" />
+            <Button icon="&#x274C;" text='' type="text" variety="default" />
+            <ButtonDanger icon="&#x274C;" text="" type="outlined" />
+            <ButtonDanger icon="&#x274C;" text='Button - lg' size='lg' />
+          </div>
+
         </div>
-
-        <h2>
-          Text
-        </h2>
-
-        <div>
-          <Button text="Text button" type="text" variety="default" classNames={[]} />
-        </div>
-
-        <h2>
-          Icon
-        </h2>
-
-        <div>
-          <Button icon="&#x274C;" text="Leading icon" type="text" variety="default" />
-          <Button icon="&#x274C;" text='' type="text" variety="default" />
-          <ButtonDanger icon="&#x274C;" text="" type="outlined"/>
-          <ButtonDanger  icon="&#x274C;"  text='Button - lg' size='lg' />
-        </div>
-
       </div>
-
-
-
-
-      <div>
-
-      </div>
-
-    </div>
+    </div >
 
 
   )
